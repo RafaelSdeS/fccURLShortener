e3a5ee0405d59e63030a35692542e6366d112c1d
Rafael <rafesilvadesouza@gmail.com>
2023-05-11 16:47:44 -0300

    Second commit

--- index.js
+++ index.js
@@ -99,59 +99,6 @@ app.get('/api/shorturl/:id', async (req, res) => {
 })
 
 
-// The actual thing
-// app.post('/api/shorturl', async (req, res) => {
-//   const url = req.body.url
-//   const urlCode = shortid.generate()
-
-//   if(!regex.test(url)) {
-//     res.status(401).json({
-//       error: 'invalid url'
-//     })
-//   } else {
-//     try {
-//       let findOne = await URL.findOne({
-//         original_url: url
-//       })
-//       if (findOne){
-//         res.json({
-//           original_url: findOne.original_url,
-//           short_url: findOne.short_url
-//         })
-//       } else {
-//         findOne = new URL({
-//           original_url: url,
-//           short_url: urlCode
-//         })
-//         await findOne.save()
-//         res.json({
-//           original_url: findOne.original_url,
-//           short_url: findOne.short_url
-//         })
-//       }
-//     } catch(err) {
-//       console.log(err)
-//       res.status(500).json("Server error'")
-//     }
-//   }
-// })
-
-// app.get('api/shorturl/:short_url?', async(req, res) => {
-//   try {
-//     const urlParams = await URL.findOne({
-//       short_url: req.params.short_url
-//     })
-//     if(urlParams) {
-//       return res.redirect(urlParams.original_url)
-//     } else {
-//       return res.status(404).json('No Url')
-//     }
-//   } catch (err) {
-//     console.log(err)
-//     res.status(500).json("Server error")
-//   }
-// })
-
 
 // App listen
 app.listen(port, function() {
